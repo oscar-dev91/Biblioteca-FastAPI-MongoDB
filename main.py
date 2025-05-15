8dfe79ef3855798d34a7415ac56062633c34b7a0
Oscar Palomino <oscar_epalomino@soy.sena.edu.co>
2025-05-15 00:15:15 -0500

    Añadida ruta y config a koyeb

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
 from routers import libro, elemento
+from typing import Union
 
 app = FastAPI(title="API Biblioteca - MongoDB")
 
 app.include_router(libro.router)
 app.include_router(elemento.router)
+
+@app.get('/')
+def read_root():
+    return {'message': 'Hello World form FastAPI and Koyeb'}
